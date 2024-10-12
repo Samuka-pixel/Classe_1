94e929850cec260b07da6a8321b0bae0de2b9b8f
Samuka-pixel <shark06778@sharkcoders.pt>
2024-10-12 09:42:41 +0100

    Add files via upload

---
+++ numeros.py
@@ -0,0 +1,11 @@
+numero1 = 6.6
+numero2 = 7.5
+numero3 =3.9
+soma =(numero1+numero3/numero2)/6*65
+print("A a soma é:", soma)
+
+print("Hello world")
+
+nome = "Samuel"
+idade ="15"
+print("Olá, meu nome é", nome,"e tenho", idade,"anos")
